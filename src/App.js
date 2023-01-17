e4062ac79775ab950e44dd2bf38f0bc51c01e1c8
deployer420 <117289105+deployer420@users.noreply.github.com>
2023-01-17 13:59:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -311,31 +311,9 @@ function App() {
 
 
             <s.TextTitle>
-              Public mint time: 1 PM UTC 17/01/2023
+              MINT OPEN
             </s.TextTitle>
 
-            <s.TextTitle>
-              Time until mint:
-            </s.TextTitle>
-
-
-            <s.TextDescription
-              style={{
-                textAlign: "center",
-                fontSize: 50,
-                fontWeight: "bold",
-                color: "var(--accent-text)",
-              }}
-            >
-              {truncate(
-                new Date(timeRemaining).toLocaleString("en-GB", {
-                  hour: "numeric",
-                  minute: "numeric",
-                  second: "numeric",
-                }),
-                10
-              )}
-            </s.TextDescription>
 
 
             <s.TextTitle>
